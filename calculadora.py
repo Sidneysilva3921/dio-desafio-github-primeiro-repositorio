681351d2d69374486e323f71607917156e3ae928
Sidneysilva3921 <114320455+Sidneysilva3921@users.noreply.github.com>
2022-09-28 12:50:49 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,22 @@
+print("calculadora")
+
+
+
+n_1 = int(input('Digite Um Numero: '))
+n_2 = int(input('Digite Um Novo Numero: '))
+
+#adicao
+print('{} + {} = '.format(n_1, n_2))
+print(n_1 + n_2)
+
+#subtrair
+print('{} - {} = '.format(n_1, n_2))
+print(n_1 - n_2)
+
+#comomultilplicar
+print('{} * {} = '.format(n_1, n_2))
+print(n_1 * n_2)
+
+#paradividir
+print('{} / {} = '.format(n_1, n_2))
+print(n_1 / n_2)
